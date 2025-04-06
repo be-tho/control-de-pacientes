bb163262aba58cd1d7b91f5cccf1af1206a8bff2
Albert Villarroel <56642267+be-tho@users.noreply.github.com>
2025-04-06 19:46:03 -0300

    upload files

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import  PatientList from "./components/PatientList"
+import  PatientForm from "./components/PatientForm"
+
+function App() {
+  return (
+    <>
+    <div className="container mx-auto mt-20">
+      <h1 className="font-black text-5xl text-center md:w-2/3 md:mx-auto ">Seguimiento de Pacientes {''}<span className="text-indigo-700 font-black">Veterinaria</span></h1>
+    
+    </div>
+    <div className="mt-12 md:flex">
+      <PatientList />
+      <PatientForm />
+    </div>
+    </>
+  )
+}
+
+export default App
