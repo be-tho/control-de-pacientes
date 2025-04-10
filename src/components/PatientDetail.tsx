e325ad162a0a7b076e3decd4efd2387785fe4346
Albert Villarroel <56642267+be-tho@users.noreply.github.com>
2025-04-09 21:38:11 -0300

    actualizar el paciente

--- src/components/PatientDetail.tsx
+++ src/components/PatientDetail.tsx
@@ -1,11 +1,16 @@
 import { Patient } from "../types"
 import PatientDetailItem from "./PatientDetailItem"
+import { usePatientStore } from "../store/store"
 
 type PatientDetailProps = {
     patient: Patient
 }
 
 export default function PatientDetail({patient}: PatientDetailProps) {
+
+    const deletePatient = usePatientStore((state) => state.deletePatient)
+    const getPatientById = usePatientStore((state) => state.getPatientById)
+
     return (
         <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl">
             <PatientDetailItem label="ID" data={patient.id} />
@@ -15,11 +20,15 @@ export default function PatientDetail({patient}: PatientDetailProps) {
             <PatientDetailItem label="Fecha Alta" data={patient.date.toString()} />
             <PatientDetailItem label="Síntomas" data={patient.symptoms} />
 
-            <div className="flex justify-between mt-10">
-                <button type="button" className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg ">
+            <div className="flex flex-col lg:flex-row gap-3 justify-between mt-10">
+                <button 
+                    type="button" 
+                    className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg cursor-pointer"
+                    onClick={() => getPatientById(patient.id)}
+                >
                     Editar
                 </button>
-                <button type="button" className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg ">
+                <button type="button" className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg cursor-pointer" onClick={() => deletePatient(patient.id)}>
                     Borrar
                 </button>
             </div>
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand"
+import { devtools } from "zustand/middleware"
 import { DraftPatient, Patient } from "../types"
 import { v4 as uuid4 } from "uuid"
 
@@ -6,7 +7,12 @@ import { v4 as uuid4 } from "uuid"
 // Types
 type PatientState = {
     patients: Patient[]
+
+    activeId: Patient['id']
     addPatient: (data:DraftPatient) => void
+    deletePatient: (id: Patient['id']) => void
+    getPatientById: (id: Patient['id']) => void
+    updatePatient: (data:DraftPatient) => void
 }
 
 // Utils
@@ -18,13 +24,41 @@ const createPatient = (patient: DraftPatient) : Patient =>{
 }
 
 // Store, cuando guarde el paciente reiniciar el form
-export const usePatientStore = create<PatientState>((set) => ({
-    patients : [],
-    addPatient: (data) => {
-        const newPatient = createPatient(data)
+export const usePatientStore = create<PatientState>()(
+    devtools((set) => ({
+        patients : [],
+        activeId: '',
 
-        set((state) => ({
-            patients: [...state.patients, newPatient]
-        }))
-    }
-}))        
+        //Funcion para agregar un paciente
+        addPatient: (data) => {
+            const newPatient = createPatient(data)
+
+            set((state) => ({
+                patients: [...state.patients, newPatient]
+            }))
+        },
+
+        //Funcion para eliminar un paciente
+        deletePatient: (id) => {
+            set((state) => ({
+                patients: state.patients.filter(patient => patient.id !== id)
+            }))
+        },
+
+        //Funcion para obtener un paciente
+        getPatientById: (id) => {
+            set(() => ({
+                activeId: id
+            }))
+        
+        },
+
+        //Funcion para actualizar un paciente
+        updatePatient: (data) => {
+            set((state) => ({
+                patients: state.patients.map(patient => patient.id === state.activeId ? {id: state.activeId, ...data} : patient),
+                activeId: ''
+            }))
+        }
+    })
+))     
